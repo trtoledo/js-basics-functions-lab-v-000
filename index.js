5f311c258050c78ac2d7c9da7922f9879713df11
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-27 20:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function distanceFromHqInBlocks(blockNumber){
+  var numberOfBlocks
+  numberOfBlocks = blockNumber - 42
+
+  if numberOfBlocks <= 1
+  return 1
+  else
+  return numberOfBlocks.round
+}
+
 // Code your solution in this file!

223b7535371e66f9359a1a34f2853bc8dbdd99b5
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-27 21:07:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function distanceFromHqInBlocks(blockNumber) {
     return Math.abs(blockNumber - 42);
 }
 
-function distanceFromHqInFeet( ){
-
-  
+function distanceFromHqInFeet(blocks){
+  return distanceFromHqInBlocks(blocks) * 264;
 }
 // Code your solution in this file!

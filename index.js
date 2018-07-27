515b5a0ac3580b24187cb1e96b08e7a7680c2cad
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-27 22:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ function calculatesFarePrice (departureBlock, arrivalBlock){
   const distanceAfter400Feet = distanceTravelledInFeet(departureBlock, arrivalBlock)
 
   switch (distanceAfter400Feet) {
-  case 'distanceAfter400Feet <= 400':
+  case '<= 400':
     return 0;
     break;
-  case 'distanceAfter400Feet > 400 && 2000':
+  case '> 400 && 2000':
     return (distanceAfter400Feet) * 0.02
     break;
-  case 'distanceAfter400Feet > 2000 && < 2500':
+  case '> 2000 && < 2500':
     return 25;
     break;
-  case 'distanceAfter400Feet < 2500':
+  case '< 2500':
     return "cannot travel that far";
     break;
 }
